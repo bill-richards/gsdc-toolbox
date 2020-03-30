37b775421d71888a170af9028111d3963d549f1b
Bill Richards <bill@greyskin.co.uk>
2020-03-30 18:09:51 +0100

    - restructured core-modules directories

---
+++ core-modules/background/gsdc.toolbox.modules.background/ViewModels/BannerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Media;
+using Prism.Mvvm;
+
+namespace gsdc.toolbox.modules.background.ViewModels
+{
+    internal class BannerViewModel : BindableBase
+    {
+        public string CompanyName
+            => "gsdc";
+
+        public string ApplicationName 
+            => "Toolbox";
+
+        public Brush CompanyNameColour
+            // ReSharper disable once PossibleNullReferenceException
+            => new SolidColorBrush{Color = (Color)ColorConverter.ConvertFromString("#FFC5BC25") };
+
+        public Brush ApplicationNameColour
+            // ReSharper disable once PossibleNullReferenceException
+            => new SolidColorBrush{Color = (Color)ColorConverter.ConvertFromString("#FFC4CFD6") };
+        
+    }
+}
