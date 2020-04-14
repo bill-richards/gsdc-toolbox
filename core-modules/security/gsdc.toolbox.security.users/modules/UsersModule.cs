33ffbbd56630c8da98652c2030a936e5826aa11d
Bill Richards <bill@greyskin.co.uk>
2020-04-14 11:22:45 +0100

    Added security assembly Added security.users module

---
+++ core-modules/security/gsdc.toolbox.security.users/modules/UsersModule.cs
@@ -0,0 +1,20 @@
+﻿using gsdc.toolbox.user.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace gsdc.toolbox.user
+{
+    public class userModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
